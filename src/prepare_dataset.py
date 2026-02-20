c930f664acf3209d94871ac757d6d8d0c4f9a67c
Patryk Gajda <patryk.gajda@marcopol.pl>
2026-02-20 19:47:44 +0100

    44

--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split
 load_dotenv()
 
 # ── Ścieżki ──────────────────────────────────────────────────────────────────
-BASE_DIR = Path(__file__).resolve().parents[2]
+BASE_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = BASE_DIR / "data"
 LOOKUP_DIR = DATA_DIR / "lookups"
 OUTPUT_DIR = DATA_DIR / "processed"
